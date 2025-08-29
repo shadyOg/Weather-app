8b63cf1ee5f0aae06b7a094f92816a8da6f945d8
Dorkenoo Shadrack <shadrackdorkenoo@gmail.com>
2025-08-29 01:28:47 +0000

    added API key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card  = document.querySelector(".card");
-const apiKey = "get your own api key at openweathermap.com";
+const apiKey = "bacd68ae7e3d60e1b43c12de34c1b3f9";
 
 weatherForm.addEventListener("submit", async event => {
 
